0cd9d6cb10f7d0a3e7daa4f37eb86b3d69e7cd13
aaronjan <aaronjan98@gmail.com>
2026-01-06 08:34:21 -0800

    add lazy.nvim config

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,27 @@
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- -------------------------
+-- Buffer navigation (s held = Alt)
+-- -------------------------
+map("n", "<M-j>", ":bnext<CR>", opts)
+map("n", "<M-k>", ":bprevious<CR>", opts)
+
+-- -------------------------
+-- Tab navigation (s held = Alt)
+-- -------------------------
+map("n", "<M-h>", ":tabprevious<CR>", opts)
+map("n", "<M-l>", ":tabnext<CR>", opts)
+
+-- -------------------------
+-- Pane navigation across Neovim splits AND tmux panes (f held = Ctrl+Alt)
+-- Requires vim-tmux-navigator plugin
+-- -------------------------
+map("n", "<C-M-h>", ":TmuxNavigateLeft<CR>", opts)
+map("n", "<C-M-j>", ":TmuxNavigateDown<CR>", opts)
+map("n", "<C-M-k>", ":TmuxNavigateUp<CR>", opts)
+map("n", "<C-M-l>", ":TmuxNavigateRight<CR>", opts)
+
+-- Save
+map("n", "<C-s>", ":w<CR>", opts)
+
---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+
+-- System clipboard (Wayland works via wl-copy installed by Nix)
+vim.opt.clipboard = "unnamedplus"
+
+-- Slightly nicer defaults
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 400
+
